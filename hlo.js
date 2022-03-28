a1df2c680cff1e7d46f5f2b1619efe4abab9326b
nantezavia <nantezafl24@gmail.com>
2022-03-28 12:54:08 +0300

    my javascript

---
+++ hlo.js
@@ -0,0 +1,8 @@
+const temp= 40;
+if( temp >=40 &&temp <= 46){
+    console.log("you are temperature is normal");
+}
+else{
+    console.log("your temperature is erratic, visit the nearest hospital");
+}
+
