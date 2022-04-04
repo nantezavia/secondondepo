be19f97c87f3c35251268491ef841c059203f651
nantezavia <nantezafl24@gmail.com>
2022-04-04 09:57:10 +0300

    my assignment

---
+++ assign.js
@@ -0,0 +1,8 @@
+// lift going up
+for(let floor=1; floor<=4; floor++){
+    console.log("lift going up")
+    if (floor==4){
+        console.log("you have arrived at your destination")
+    };
+};
+    
